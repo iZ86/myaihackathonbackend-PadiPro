d87909d9ed6c7c6729af4644785182edff0d635b
iZ86 <iZLikesCS@gmail.com>
2026-04-07 14:17:44 +0800

    Add cors for frontend

--- app/index.ts
+++ app/index.ts
@@ -19,6 +19,7 @@ export default class Server {
 
   private config(app: Application): void {
     const corsOptions: CorsOptions = {
+      origin: "http://localhost:5173"
     };
     app.use(cors(corsOptions));
 
