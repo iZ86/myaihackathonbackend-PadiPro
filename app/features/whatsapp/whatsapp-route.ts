8934ac30777392131a79d937f8f7b53951e91ac3
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2026-04-09 23:11:19 +0800

    initialize whatsapp feature

---
+++ app/features/whatsapp/whatsapp-route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { Router } from "express";
+import WhatsappController from './whatsapp-controller';
+
+
+class WhatsappRoute {
+    router = Router();
+    controller = new WhatsappController();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    initializeRoutes() {
+        const app = express(); app.use(express.json()); 
+        const verifyToken = process.env.WHATSAPP_VERIFY_TOKEN;
+
+        this.router.get('/', (req, res) => {
+        const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+
+        if (mode === 'subscribe' && token === verifyToken) {
+            console.log('WEBHOOK VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.status(403).end();
+        }
+        });
+    } 
+}
+
+export default new WhatsappRoute().router;
--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Application } from "express";
 import userRoute from "../features/user/user-route";
 import geminiRoute from "../features/gemini/gemini-route";
+import whatsappRoute from "../features/gemini/gemini-route";
 
 export default class Routes {
   constructor(app: Application) {
     app.use("/api/v1/users", userRoute)
     app.use("/api/v1/gemini", geminiRoute)
+    app.use("/", whatsappRoute)
   }
 }
