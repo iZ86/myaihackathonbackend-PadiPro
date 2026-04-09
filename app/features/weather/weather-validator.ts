216821b88561af2199bbf8aecac6b8fedf219331
iZ86 <iZLikesCS@gmail.com>
2026-04-10 00:16:19 +0800

    Fix weatherParamValidator to check param

--- app/features/weather/weather-validator.ts
+++ app/features/weather/weather-validator.ts
@@ -1,7 +1,13 @@
 import { body, param, query } from "express-validator";
 import validate from "../../middlewares/validate";
 
+
 export const weatherParamValidator: any = [
+  param('mobile_no')
+    .notEmpty().withMessage("Missing mobile_no.")
+    .isString().withMessage("mobile_no must be set"),
+  validate,
+];
   body('mobile_no')
     .exists().withMessage("Missing mobile_no.")
     .isString().withMessage("mobile_no must be set"),
