f9b649575543ab03f4f54ab8d547607ed3702ced
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2026-04-10 01:13:07 +0800

    testing connection

--- app/features/whatsapp/whatsapp-route.ts
+++ app/features/whatsapp/whatsapp-route.ts
@@ -26,6 +26,13 @@ class WhatsappRoute {
 
             return res.status(403).end();
         });
+
+        this.router.post('/', (req, res) => {
+            const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+            console.log(`\n\nWebhook received ${timestamp}\n`);
+            console.log(JSON.stringify(req.body, null, 2));
+            res.status(200).end();
+        })
     } 
 }
 
