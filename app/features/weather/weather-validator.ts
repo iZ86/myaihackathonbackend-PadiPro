6c59de3a499dd7643d0b95282d4441dcc83467ea
iZ86 <iZLikesCS@gmail.com>
2026-04-10 00:16:03 +0800

    Remove example comment in weather-validator

--- app/features/weather/weather-validator.ts
+++ app/features/weather/weather-validator.ts
@@ -1,8 +1,6 @@
 import { body, param, query } from "express-validator";
 import validate from "../../middlewares/validate";
 
-/** Used for 400 validation checks. */
-/** May be used in other domains. */
 export const weatherParamValidator: any = [
   body('mobile_no')
     .exists().withMessage("Missing mobile_no.")
