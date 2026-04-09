80740930225fce272f652d2c961664799fd89166
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2026-04-10 00:48:25 +0800

    testing connection

--- app/features/whatsapp/whatsapp-route.ts
+++ app/features/whatsapp/whatsapp-route.ts
@@ -15,20 +15,8 @@ class WhatsappRoute {
         const verifyToken = process.env.WHATSAPP_VERIFY_TOKEN;
 
         this.router.get('/', (req, res) => {
-            console.log("META HIT RECEIVED:", req.query);
-
-            const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
-
-            console.log("MODE:", mode);
-            console.log("TOKEN:", token);
-            console.log("EXPECTED:", process.env.WHATSAPP_VERIFY_TOKEN);
-
-            if (mode === 'subscribe' && token === process.env.WHATSAPP_VERIFY_TOKEN) {
-                console.log('WEBHOOK VERIFIED');
-                return res.status(200).send(challenge);
-            }
-
-            return res.status(403).end();
+            console.log("🔥 ROUTE HIT");
+            res.status(200).send("OK");
         });
     } 
 }
