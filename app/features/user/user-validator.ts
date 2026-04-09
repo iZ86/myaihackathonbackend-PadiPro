331ed0451ada6e533abfefddaf116a691ea7d48e
iZ86 <iZLikesCS@gmail.com>
2026-04-10 00:15:29 +0800

    Modify userParamValidator to use notEmpty instead of exist

--- app/features/user/user-validator.ts
+++ app/features/user/user-validator.ts
@@ -1,9 +1,10 @@
 import { body, param, query } from "express-validator";
 import validate from "../../middlewares/validate";
 
+
 export const userParamValidator: any = [
   param('mobile_no')
-    .exists().withMessage("Missing mobile_no.")
+    .notEmpty().withMessage("Missing mobile_no.")
     .isInt().withMessage("mobile_no must be a set"),
   validate,
 ];
