eb0af825cd1c9f6e6bca9671780fdbf650cb0282
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2026-04-11 13:02:53 +0800

    add back original log to make sure msg received

--- app/features/whatsapp/whatsapp-route.ts
+++ app/features/whatsapp/whatsapp-route.ts
@@ -14,6 +14,9 @@ class WhatsappRoute {
 
     initializeRoutes() {
         this.router.get('/', (req, res) => {
+            const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+            console.log(`\n\nWebhook received ${timestamp}\n`);
+
             const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
             if (mode === 'subscribe' && token === process.env.WHATSAPP_VERIFY_TOKEN) {
