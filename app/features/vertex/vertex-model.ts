b3613ddf1d0b99393d29bd60876153f2cd135e79
iZ86 <iZLikesCS@gmail.com>
2026-04-13 00:43:26 +0800

    Add vertex-model.ts and VertexSessionData Model

---
+++ app/features/vertex/vertex-model.ts
@@ -0,0 +1,29 @@
+export interface VertexSessionData {
+  results: {
+    id: string,
+    document: {
+      name: string,
+      id: string,
+      derivedStructData: {
+        snippets: {
+          snippet_status: string,
+          snippet: string
+        }[],
+        title: string,
+        link: string,
+        can_fetch_raw_content: string
+      }
+    }
+  }[],
+  totalSize: number,
+  attributionToken: string,
+  nextPageToken: string,
+  guidedSearchResult: {},
+  summary: {},
+  queryExpansionInfo: {},
+  sessionInfo: {
+    name: string,
+    queryId: string
+  },
+  semanticState: string
+}
