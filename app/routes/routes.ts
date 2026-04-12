ded83601ffe4646687c6ea5026fcf0cd2a04849d
iZ86 <iZLikesCS@gmail.com>
2026-04-13 00:46:55 +0800

    Add vertexRoute to routes.ts under api/v1/vertex

--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -2,11 +2,13 @@ import { Application } from "express";
 import userRoute from "../features/user/user-route";
 import weatherRoute from "../features/weather/weather-route";
 import geminiRoute from "../features/gemini/gemini-route";
+import vertexRoute from "../features/vertex/vertex-route";
 
 export default class Routes {
   constructor(app: Application) {
     app.use("/api/v1/users", userRoute)
     app.use("/api/v1/weather", weatherRoute)
     app.use("/api/v1/gemini", geminiRoute)
+    app.use("/api/v1/vertex", vertexRoute);
   }
 }
