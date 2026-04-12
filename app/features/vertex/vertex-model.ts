e6beacd94fda32e4cfb4cc372af939129c38e04f
iZ86 <iZLikesCS@gmail.com>
2026-04-13 00:48:39 +0800

    Add VertexAnswerData model

--- app/features/vertex/vertex-model.ts
+++ app/features/vertex/vertex-model.ts
@@ -28,6 +28,52 @@ export interface VertexSessionData {
   semanticState: string
 }
 
+export interface VertexAnswerData {
+  answer: {
+    name: string;
+    state: string;
+    answerText: string;
+    citations: {
+      startIndex: string;
+      endIndex: string;
+      sources: {
+        referenceId: string;
+      }[];
+    }[];
+    references: {
+      chunkInfo: {
+        content: string;
+        revelanceScore: number;
+        documentMetaData: {
+          document: string;
+          uri: string;
+          title: string;
+        }
+      }
+    }[];
+    relatedQuestions: string[];
+    queryUnderstandingInfo: {
+      queryClassificationInfo: {
+        type: string;
+      }[];
+    };
+  };
+  session: {
+    name: string;
+    state: string;
+    userPseudoId: string;
+    turns: {
+      query: {
+        queryId: string;
+        text: string;
+      };
+      answer: string;
+    }[];
+    startTime: string;
+    endTime: string;
+  };
+  answerQueryToken: string;
+}
 export interface VertexSessionInfoData {
   session: string;
   queryId: string;
