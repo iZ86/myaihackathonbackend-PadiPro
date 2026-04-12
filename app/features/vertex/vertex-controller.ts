5dcf36e6d9ae2d20dbdac990bc93fa7e8dcba70e
iZ86 <iZLikesCS@gmail.com>
2026-04-13 00:49:47 +0800

    Add controller sendQueryVertex

--- app/features/vertex/vertex-controller.ts
+++ app/features/vertex/vertex-controller.ts
@@ -1,13 +1,25 @@
 import { Request, Response } from "express";
 import { Result } from "../../../libs/Result";
 import vertexService from "./vertex-service";
-import { VertexSessionInfoData } from "./vertex-model";
+import { VertexAnswerQueryData, VertexSessionInfoData } from "./vertex-model";
 
 
 export default class VertexController {
 
   async createVertexSession(req: Request, res: Response) {
     const result: Result<VertexSessionInfoData> = await vertexService.createVertexSession();
+    if (result.isSuccess()) {
+      return res.sendResponse(result.getStatusCode(), result.getMessage(), result.getData());
+    } else if (result.isFailure()) {
+      return res.sendResponse(result.getStatusCode(), result.getMessage());
+    }
+  }
+
+  async sendQueryVertex(req: Request, res: Response) {
+    const text: string = req.body.text;
+    const session: string = req.body.session;
+
+    const result: Result<VertexAnswerQueryData> = await vertexService.sendQueryVertex(text, session);
     if (result.isSuccess()) {
       return res.sendResponse(result.getStatusCode(), result.getMessage(), result.getData());
     } else if (result.isFailure()) {
