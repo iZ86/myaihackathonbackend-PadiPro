6adbf651ef8aa55663275611b833d6d76e7f1f8f
iZ86 <iZLikesCS@gmail.com>
2026-04-18 15:46:05 +0800

    Add endpoint PUT users/:mobile_no/coords

--- app/features/user/user-route.ts
+++ app/features/user/user-route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { asyncHandler } from "../../utils/utils";
 import UserController from "./user-controller";
 import { checkAuthTokenHeader } from "../../middlewares/auth";
-import { userParamValidator, createUserBodyValidator } from "./user-validator"
+import { userParamValidator, createUserBodyValidator, updateUserCoordsByMobileNoBodyValidator } from "./user-validator"
 
 
 class UserRoute {
@@ -18,6 +18,8 @@ class UserRoute {
     this.router.get("/:mobile_no", checkAuthTokenHeader, userParamValidator, asyncHandler(this.controller.getUserByMobileNo));
 
     this.router.post("/", checkAuthTokenHeader, createUserBodyValidator, asyncHandler(this.controller.createUser));
+
+    this.router.put("/:mobile_no/coords", checkAuthTokenHeader, userParamValidator, updateUserCoordsByMobileNoBodyValidator, asyncHandler(this.controller.updateUserCoordsByMobileNo));
   }
 }
 
