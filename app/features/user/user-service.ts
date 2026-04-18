f15cda050f7361176551751a92f65f6c010786d4
iZ86 <iZLikesCS@gmail.com>
2026-04-18 15:43:27 +0800

    Add service createUser

--- app/features/user/user-service.ts
+++ app/features/user/user-service.ts
@@ -7,6 +7,7 @@ import userRepository from "./user-repository";
 interface IUserService {
   getUsers(): Promise<Result<UserData[]>>;
   getUserByMobileNo(mobile_no: string): Promise<Result<UserData>>;
+  createUser(mobile_no: string, name: string): Promise<Result<UserData>>;
 }
 
 class UserService implements IUserService {
@@ -24,6 +25,26 @@ class UserService implements IUserService {
     }
 
     return Result.succeed(ENUM_STATUS_CODES_SUCCESS.OK, user, `User with mobile_no ${mobile_no} found.`);
+  }
+
+  public async createUser(mobile_no: string, name: string): Promise<Result<UserData>> {
+    const userResult: Result<UserData> = await this.getUserByMobileNo(mobile_no);
+    if (userResult.isSuccess()) {
+      return Result.fail(ENUM_STATUS_CODES_FAILURE.CONFLICT, "User already exist with mobile_no.");
+    }
+
+    const createUserResult: boolean = await userRepository.createUser(mobile_no, name);
+    if (!createUserResult) {
+      throw new Error("createUser failed to create user.")
+    }
+
+    const user: Result<UserData> = await this.getUserByMobileNo(mobile_no);
+
+    if (user.isFailure()) {
+      throw new Error("createUser failed to get created user.");
+    }
+
+    return Result.succeed(ENUM_STATUS_CODES_SUCCESS.CREATED, user.getData(), "User successfully created.");
   }
 }
 
