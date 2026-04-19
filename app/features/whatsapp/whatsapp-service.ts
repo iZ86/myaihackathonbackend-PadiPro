195aab1c0a85bd40966b1ab613b8ef57539f4af0
iZ86 <iZLikesCS@gmail.com>
2026-04-20 07:59:34 +0800

    Refactor service myHandleText

--- app/features/whatsapp/whatsapp-service.ts
+++ app/features/whatsapp/whatsapp-service.ts
@@ -241,78 +241,11 @@ export class WhatsappService {
 
     const mobile_no: string = user.mobile_no;
 
-    const getWeatherResult: Result<WeatherData> = await weatherService.getWeatherByMobileNo(mobile_no);
+    await this.syncUserWeather(mobile_no);
 
-    if (getWeatherResult.isSuccess()) {
+    let weatherQuery: string = await this.generateWeatherQuery(mobile_no);
 
-      const updateWeatherResult: Result<WeatherData> = await weatherService.updateWeather(mobile_no);
-
-      if (updateWeatherResult.isFailure() &&
-        ((updateWeatherResult.getStatusCode() === ENUM_STATUS_CODES_FAILURE.NOT_FOUND && updateWeatherResult.getMessage() !== "User location is not set.") ||
-          (updateWeatherResult.getStatusCode() !== ENUM_STATUS_CODES_FAILURE.TOO_MANY_REQUESTS))) {
-
-        throw new Error(`handleText had issue with updateWeather: ${updateWeatherResult.getMessage()}`);
-      }
-
-
-    } else if (getWeatherResult.isFailure() && getWeatherResult.getMessage() === "User weather not found.") {
-
-      const saveWeatherResult: Result<WeatherData> = await weatherService.saveWeather(mobile_no);
-
-      if (saveWeatherResult.isFailure() && saveWeatherResult.getMessage() !== "User location is not set.") {
-        throw new Error(`handleText had issue with saveWeather: ${saveWeatherResult.getMessage()}`);
-      }
-
-    } else {
-      throw new Error("handleText failed to get weather.");
-    }
-
-
-    const weatherResult: Result<WeatherData> = await weatherService.getWeatherByMobileNo(mobile_no);
-
-    let weatherQuery: string = "";
-
-
-    if (weatherResult.isSuccess()) {
-      const weather: WeatherData = weatherResult.getData();
-      const condition = weather.weatherCondition?.replace(/_/g, " ").toLowerCase();
-      const temp = `${weather.temperature?.degrees}°${weather.temperature?.unit === "CELSIUS" ? "C" : "F"}`;
-      const feelsLike = weather.dewPoint ? `dew point ${weather.dewPoint.degrees}°C` : "";
-      const humidity = `humidity ${weather.relativeHumidity}%`;
-      const wind = `wind ${weather.wind?.speed?.value} ${weather.wind?.speed?.unit?.replace(/_/g, " ").toLowerCase()} from ${weather.wind?.direction?.cardinal?.replace(/_/g, " ")}`;
-      const gusts = weather.wind?.gust?.value ? `, gusting to ${weather.wind.gust.value} km/h` : "";
-      const cloud = `cloud cover ${weather.cloudCover}%`;
-      const rainChance = `${weather.precipitation?.probability?.percent}% chance of ${weather.precipitation?.probability?.type?.toLowerCase()}`;
-      const thunderChance = (weather.thunderstormProbability ? weather.thunderstormProbability > 0 : false) ? `, ${weather.thunderstormProbability}% chance of thunderstorms` : "";
-      const qpf = (weather.precipitation?.qpf.quantity ? weather.precipitation?.qpf?.quantity > 0 : false)
-        ? `, expected rainfall ${weather.precipitation?.qpf.quantity} ${weather.precipitation?.qpf.unit.toLowerCase()}`
-        : "";
-
-
-      weatherQuery =
-        "\nAdditionally, here are teh current weather conditions that yo may reference: " +
-        `\nCurrent weather conditions:` +
-        `\n- Condition: ${condition}` +
-        `\n- Temperature: ${temp}, ${feelsLike}, ${humidity}` +
-        `\n- Wind: ${wind}${gusts}` +
-        `\n- Sky: ${cloud}` +
-        `\n- Precipitation: ${rainChance}${thunderChance}${qpf}`;
-    } else if (weatherResult.isFailure() && weatherResult.getMessage() !== "User weather not found.") {
-      // If the weather is still not set by here. Means that the user has no location set.
-      // Hence, don't throw error unless its something else other than no user location set.
-      throw new Error(`handleText could not getWeather due to other reasons: ${weatherResult.getMessage()}`);
-    }
-
-
-    const session = userVertexSession[mobile_no] ?? await (async () => {
-      const createVertexSessionResult: Result<VertexSessionInfoData> = await vertexService.createVertexSession();
-      if (createVertexSessionResult.isSuccess()) {
-        const vertexSession: VertexSessionInfoData = createVertexSessionResult.getData();
-        userVertexSession[mobile_no] = vertexSession.session;
-        return vertexSession.session;
-      }
-      throw new Error("handleText failed to create vertex session.");
-    })();
+    const session: string = await this.getOrCreateVertexSession(mobile_no);
 
     console.log(`[text] from ${user.mobile_no}: ${msg + weatherQuery}`);
     const sendQueryVertexResult: Result<VertexAnswerQueryData> = await vertexService.sendQueryVertex(msg + weatherQuery, session);
