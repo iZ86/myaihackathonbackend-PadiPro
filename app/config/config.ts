798bba188de87fd6a93e53ce8d1f6ead09e18e56
iZ86 <iZLikesCS@gmail.com>
2026-04-22 22:53:51 +0800

    Add config vertexServiceConfig VERTEX_PROJECT_ID..
    , VERTEX_LOCATION, VERTEX_COLLECTION, VERTEX_ENGINE_ID, VERTEX_SERVING_CONFIG, to be used by discoveryengine

--- app/config/config.ts
+++ app/config/config.ts
@@ -17,6 +17,11 @@ export const vertexServiceConfig = {
   VERTEX_SESSION_URL: process.env.VERTEX_SESSION_URL as string,
   VERTEX_CREATE_SESSION_URL: process.env.VERTEX_CREATE_SESSION_URL as string,
   VERTEX_SEND_QUERY_URL: process.env.VERTEX_SEND_QUERY_URL as string,
+  VERTEX_PROJECT_ID: process.env.VERTEX_PROJECT_ID as string,
+  VERTEX_LOCATION: process.env.VERTEX_LOCATION as string,
+  VERTEX_COLLECTION: process.env.VERTEX_COLLECTION as string,
+  VERTEX_ENGINE_ID: process.env.VERTEX_ENGINE_ID as string,
+  VERTEX_SERVING_CONFIG: process.env.VERTEX_SERVING_CONFIG as string,
   VERTEX_PROMPT_SEC: process.env.VERTEX_PROMPT_SEC as string,
   VERTEX_MODEL_VERSION: process.env.VERTEX_MODEL_VERSION as string
 }
