923764d485619716f6d83a6f77aa8097659626ef
iZ86 <iZLikesCS@gmail.com>
2026-04-22 23:10:11 +0800

    Remove model VertexSessionData
    no longer used

--- app/features/vertex/vertex-model.ts
+++ app/features/vertex/vertex-model.ts
@@ -1,33 +1,3 @@
-export interface VertexSessionData {
-  results: {
-    id: string,
-    document: {
-      name: string,
-      id: string,
-      derivedStructData: {
-        snippets: {
-          snippet_status: string,
-          snippet: string
-        }[],
-        title: string,
-        link: string,
-        can_fetch_raw_content: string
-      }
-    }
-  }[],
-  totalSize: number,
-  attributionToken: string,
-  nextPageToken: string,
-  guidedSearchResult: {},
-  summary: {},
-  queryExpansionInfo: {},
-  sessionInfo: {
-    name: string,
-    queryId: string
-  },
-  semanticState: string
-}
-
 export interface VertexAnswerData {
   answer: {
     name: string;
