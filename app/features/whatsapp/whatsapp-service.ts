69ef3f7e0ee5a87f7240b50e10a2ddca1f6a0e71
Jimmerino <jimmerinocode@gmail.com>
2026-04-24 01:15:30 +0000

    remove sendOTP in handleAudio

--- app/features/whatsapp/whatsapp-service.ts
+++ app/features/whatsapp/whatsapp-service.ts
@@ -523,7 +523,6 @@ export class WhatsappService {
 
   private async handleAudio(msg: IAudioMessage, user: UserData): Promise<void> {
     await this.reply.sendText(msg.from, "Sorry, I can’t process audio or video messages. Please send your question as text or an image.");
-    await this.sendOTP(msg.from, "123456");
   }
 
   private async handleVideo(msg: IVideoMessage, user: UserData): Promise<void> {
