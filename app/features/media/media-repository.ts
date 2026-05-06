8df00376034ac89f1e394128242fc6365e7f0190
iZ86 <iZLikesCS@gmail.com>
2026-05-06 15:31:13 +0800

    Add repo getImagesAndVideosMetaDataByMobileNo

--- app/features/media/media-repository.ts
+++ app/features/media/media-repository.ts
@@ -1,7 +1,31 @@
+import { db } from '../../database/db-connection';
+import { MediaData } from './media-model';
+
 interface IMediaRepository {
+  getImagesAndVideosMetaDataByMobileNo(mobile_no: string): Promise<MediaData[]>;
 }
+
+
 class MediaRepository implements IMediaRepository {
   private readonly collection: string = 'medias';
+
+  public async getImagesAndVideosMetaDataByMobileNo(mobile_no: string): Promise<MediaData[]> {
+    try {
+      const snapshot = await db.collection(this.collection)
+        .where('from', '==', mobile_no)
+        .where('mimeType', 'in', ['image/jpeg', 'image/png', 'image/webp', 'video/mp4'])
+        .get();
+
+      if (snapshot.empty) return [];
+
+      return snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as unknown as MediaData[];
+    } catch (error) {
+      throw new Error(`getMediasByMobileNo repository error`, { cause: error });
+    }
+  }
 }
 
 export default new MediaRepository();
