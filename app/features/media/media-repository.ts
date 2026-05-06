e83b39958128d4d990a9299d2dd7c1619aa931c6
iZ86 <iZLikesCS@gmail.com>
2026-05-06 15:32:37 +0800

    Add repo updateImageDiagnosis

--- app/features/media/media-repository.ts
+++ app/features/media/media-repository.ts
@@ -1,9 +1,11 @@
 import { db } from '../../database/db-connection';
 import { MediaData } from './media-model';
+import { ImageOutputDetection } from '../gemini/gemini-model';
 
 interface IMediaRepository {
   getImagesAndVideosMetaDataByMobileNo(mobile_no: string): Promise<MediaData[]>;
   getMediaMetaDataByMediaName(mediaName: string): Promise<MediaData | undefined>;
+  updateImageDiagnosis(mediaName: string, detections: Array<ImageOutputDetection>): Promise<boolean>;
 }
 
 
@@ -43,6 +45,32 @@ class MediaRepository implements IMediaRepository {
       return { id: doc.id, ...doc.data() } as unknown as MediaData;
     } catch (error) {
       throw new Error(`getMediaByMediaName repository error`, { cause: error });
+    }
+  }
+
+  public async updateImageDiagnosis(mediaName: string, detections: Array<ImageOutputDetection>,): Promise<boolean> {
+    try {
+      const snapshot = await db.collection(this.collection)
+        .where('mediaName', '==', mediaName)
+        .limit(1)
+        .get();
+
+      if (snapshot.empty) {
+        console.warn(`No record found for media_id: ${mediaName}`);
+        return false;
+      }
+
+      const doc = snapshot.docs[0];
+      if (!doc) return false;
+
+      await doc.ref.update({
+        detections: detections,
+        updatedAt: new Date().toISOString()
+      });
+
+      return true;
+    } catch (error) {
+      throw new Error(`updateImageDiagnosis repository error`, { cause: error });
     }
   }
 }
