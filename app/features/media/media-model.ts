b5c00db028aea9e4f289ec6b69002289ace6393b
iZ86 <iZLikesCS@gmail.com>
2026-05-06 15:30:53 +0800

    Add MediaData model

---
+++ app/features/media/media-model.ts
@@ -0,0 +1,10 @@
+export interface MediaData {
+  from: string,
+  mediaName: string,
+  mimeType: string,
+  storage_path: string,
+  download_url: string,
+  created_at: string,
+  caption?: string,
+  sha256?: string,
+}
