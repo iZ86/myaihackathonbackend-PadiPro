a9769dd158edc1187ef2c513b0faf8023c6e863b
iZ86 <iZLikesCS@gmail.com>
2026-05-06 15:31:41 +0800

    Add repo getMediaMetaDataByMediaName

--- app/features/media/media-repository.ts
+++ app/features/media/media-repository.ts
@@ -3,6 +3,7 @@ import { MediaData } from './media-model';
 
 interface IMediaRepository {
   getImagesAndVideosMetaDataByMobileNo(mobile_no: string): Promise<MediaData[]>;
+  getMediaMetaDataByMediaName(mediaName: string): Promise<MediaData | undefined>;
 }
 
 
@@ -24,6 +25,24 @@ class MediaRepository implements IMediaRepository {
       })) as unknown as MediaData[];
     } catch (error) {
       throw new Error(`getMediasByMobileNo repository error`, { cause: error });
+    }
+  }
+
+  public async getMediaMetaDataByMediaName(mediaName: string): Promise<MediaData | undefined> {
+    try {
+      const snapshot = await db.collection(this.collection)
+        .where('mediaName', '==', mediaName)
+        .limit(1)
+        .get();
+
+      if (snapshot.empty) return undefined;
+
+      const doc = snapshot.docs[0];
+      if (!doc) return undefined;
+
+      return { id: doc.id, ...doc.data() } as unknown as MediaData;
+    } catch (error) {
+      throw new Error(`getMediaByMediaName repository error`, { cause: error });
     }
   }
 }
