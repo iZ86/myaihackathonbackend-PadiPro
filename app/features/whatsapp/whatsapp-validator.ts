a77d4c21b86b6f1d2ff220a53668f49cc9dd55b7
iZ86 <iZLikesCS@gmail.com>
2026-05-08 00:36:44 +0800

    Add verifyOTPBodyValidator

--- app/features/whatsapp/whatsapp-validator.ts
+++ app/features/whatsapp/whatsapp-validator.ts
@@ -8,3 +8,16 @@ export const generateOTPBodyValidator: any = [
     .isString().withMessage("mobile_no must be a string."),
   validate,
 ];
+
+export const verifyOTPBodyValidator: any = [
+  body('mobile_no')
+    .trim()
+    .notEmpty().withMessage("Missing mobile_no.")
+    .isString().withMessage("mobile_no must be a string."),
+  body('otp')
+    .trim()
+    .notEmpty().withMessage("Missing otp.")
+    .isString().withMessage("otp must be a string.")
+    .isLength({ min: 6, max: 6 }).withMessage("otp must be 6 characters."),
+  validate,
+];
