06d09c783c027ef6329b14ec1f4abc8546c4a206
Jim <141894353+jimmerino2@users.noreply.github.com>
2026-05-07 12:15:30 +0800

    ref: Whatsapp service merge all services into one singular WhatsappService

--- app/features/whatsapp/whatsapp-service.ts
+++ app/features/whatsapp/whatsapp-service.ts
@@ -18,10 +18,12 @@ import {
     SendReplyResponse,
 } from "./whatsapp-model";
 import whatsappConverter from "./whatsapp-converter";
-import mainService from "../main/main-service";
 import { LocationTutorialImages, MediaData } from "../media/media-model";
 import mediaService from "../media/media-service";
+import { ChatInput } from "../chat/chat-model";
+import chatService from "../chat/chat-service";
 import { Document, ImageRun, Packer, Paragraph, HeadingLevel, BorderStyle, TextRun } from "docx";
+import mainService from "../main/main-service";
 
 interface Timeline {
     day: string;
@@ -29,8 +31,11 @@ interface Timeline {
     description: string;
 }
 
-//downloading img sent by user and saved into buffer
-export class MediaService {
+export class WhatsappService {
+    private readonly baseUrl = "https://graph.facebook.com";
+    private readonly apiVersion = process.env.WHATSAPP_API_VERSION ?? "v25.0";
+
+    //downloading img sent by user and saved into buffer
     async fetch(mediaId: string, url: string): Promise<Buffer> {
         const response = await fetch(url, {
             headers: { Authorization: `Bearer ${process.env.WHATSAPP_API_KEY}` },
@@ -38,17 +43,73 @@ export class MediaService {
         if (!response.ok) throw new Error(`Media fetch failed: ${response.status}`);
         return Buffer.from(await response.arrayBuffer());
     }
-}
 
-//sending text to user
-export class ReplyService {
-    private readonly baseUrl = "https://graph.facebook.com";
-    private readonly apiVersion = process.env.WHATSAPP_API_VERSION ?? "v25.0";
+    // Parse raw message form Whatsapp to reusable format (WhatsappMessage)
+    parse(rawMsg: RawMessage, contact: RawContact | undefined, meta: RawMetadata | undefined): WhatsappMessage {
+        const base = {
+            from: rawMsg.from,
+            messageId: rawMsg.id,
+            timestamp: rawMsg.timestamp,
+            name: contact?.profile?.name,
+            waId: contact?.wa_id,
+            phoneNumberId: meta?.phone_number_id,
+        };
 
+        switch (rawMsg.type) {
+            case "text":
+                return { ...base, type: "text", body: rawMsg.text?.body } satisfies ITextMessage;
+
+            case "image":
+                return {
+                    ...base,
+                    type: "image",
+                    mediaId: rawMsg.image?.id,
+                    url: rawMsg.image?.url,
+                    caption: rawMsg.image?.caption,
+                    mimeType: rawMsg.image?.mime_type,
+                    sha256: rawMsg.image?.sha256,
+                } satisfies IImageMessage;
+
+            case "audio":
+                return {
+                    ...base,
+                    type: "audio",
+                    mediaId: rawMsg.audio?.id,
+                    url: rawMsg.audio?.url,
+                    mimeType: rawMsg.audio?.mime_type,
+                    voice: rawMsg.audio?.voice,
+                    sha256: rawMsg.audio?.sha256,
+                } satisfies IAudioMessage;
+
+            case "video":
+                return {
+                    ...base,
+                    type: "video",
+                    mediaId: rawMsg.video?.id,
+                    url: rawMsg.video?.url,
+                    mimeType: rawMsg.video?.mime_type,
+                    sha256: rawMsg.video?.sha256,
+                } satisfies IVideoMessage;
+
+            case "location":
+                return {
+                    ...base,
+                    type: "location",
+                    latitude: rawMsg.location?.latitude,
+                    longitude: rawMsg.location?.longitude,
+                    locName: rawMsg.location?.name,
+                    address: rawMsg.location?.address,
+                } satisfies ILocationMessage;
+
+            default:
+                throw new Error(`Unsupported message type: ${(rawMsg as RawMessage).type}`);
+        }
+    }
+
+    // Send message base function
     private get endpoint(): string {
         return `${this.baseUrl}/${this.apiVersion}/${process.env.PHONE_NUMBER_ID}/messages`;
     }
-
     private async post(
         payload: SendTextPayload | SendImagePayload | SendAudioPayload | SendVideoPayload | SendDocPayload,
     ): Promise<SendReplyResponse> {
@@ -151,109 +212,16 @@ export class ReplyService {
         return this.post(payload);
     }
 
-    async sendDoc(
-        to: string,
-        source: { mediaId: string; link?: never } | { link: string; mediaId?: never },
-        options?: { caption?: string; filename?: string },
-    ): Promise<SendReplyResponse> {
-        const document =
-            "mediaId" in source && source.mediaId
-                ? {
-                      id: source.mediaId,
-                      ...(options?.caption ? { caption: options.caption } : {}),
-                      ...(options?.filename ? { filename: options.filename } : {}),
-                  }
-                : {
-                      link: source.link!,
-                      ...(options?.caption ? { caption: options.caption } : {}),
-                      ...(options?.filename ? { filename: options.filename } : {}),
-                  };
-        const payload: SendDocPayload = {
-            messaging_product: "whatsapp",
-            recipient_type: "individual",
-            to,
-            type: "document",
-            document,
-        };
-        return this.post(payload);
-    }
-}
-
-//parsing msg
-export class WhatsappService {
-    private readonly media: MediaService;
-    private readonly reply: ReplyService;
-
-    constructor() {
-        this.media = new MediaService();
-        this.reply = new ReplyService();
-    }
-
-    parse(rawMsg: RawMessage, contact: RawContact | undefined, meta: RawMetadata | undefined): WhatsappMessage {
-        const base = {
-            from: rawMsg.from,
-            messageId: rawMsg.id,
-            timestamp: rawMsg.timestamp,
-            name: contact?.profile?.name,
-            waId: contact?.wa_id,
-            phoneNumberId: meta?.phone_number_id,
-        };
-
-        switch (rawMsg.type) {
-            case "text":
-                return { ...base, type: "text", body: rawMsg.text?.body } satisfies ITextMessage;
-
-            case "image":
-                return {
-                    ...base,
-                    type: "image",
-                    mediaId: rawMsg.image?.id,
-                    url: rawMsg.image?.url,
-                    caption: rawMsg.image?.caption,
-                    mimeType: rawMsg.image?.mime_type,
-                    sha256: rawMsg.image?.sha256,
-                } satisfies IImageMessage;
-
-            case "audio":
-                return {
-                    ...base,
-                    type: "audio",
-                    mediaId: rawMsg.audio?.id,
-                    url: rawMsg.audio?.url,
-                    mimeType: rawMsg.audio?.mime_type,
-                    voice: rawMsg.audio?.voice,
-                    sha256: rawMsg.audio?.sha256,
-                } satisfies IAudioMessage;
-
-            case "video":
-                return {
-                    ...base,
-                    type: "video",
-                    mediaId: rawMsg.video?.id,
-                    url: rawMsg.video?.url,
-                    mimeType: rawMsg.video?.mime_type,
-                    sha256: rawMsg.video?.sha256,
-                } satisfies IVideoMessage;
-
-            case "location":
-                return {
-                    ...base,
-                    type: "location",
-                    latitude: rawMsg.location?.latitude,
-                    longitude: rawMsg.location?.longitude,
-                    locName: rawMsg.location?.name,
-                    address: rawMsg.location?.address,
-                } satisfies ILocationMessage;
-
-            default:
-                throw new Error(`Unsupported message type: ${(rawMsg as RawMessage).type}`);
-        }
-    }
-
-    async handle(message: WhatsappMessage, user: UserData, newUser: boolean, hasLocation: boolean): Promise<void> {
+    async handle(
+        message: WhatsappMessage,
+        user: UserData,
+        newUser: boolean,
+        hasLocation: boolean,
+    ): Promise<ChatInput | void> {
         //im directly calling this send text here to make sure we can send out this msg even if the first msg the user sents out is any media
         if (newUser) {
             await this.sendIntroductionMessage(message);
+            return;
         }
         if (message.type != "location" && !hasLocation) {
             await this.sendLocationInstructionMessage(user.mobile_no);
@@ -274,15 +242,12 @@ export class WhatsappService {
                     return this.handleLocation(message, user);
             }
         } catch (error) {
-            await this.reply.sendText(
-                message.from,
-                "We seem to be having some issues, please try again in an hour or so.",
-            );
+            await this.sendText(message.from, "We seem to be having some issues, please try again in an hour or so.");
             throw error;
         }
     }
 
-    private async handleText(msg: ITextMessage, user: UserData): Promise<void> {
+    private async handleText(msg: ITextMessage, user: UserData): Promise<ChatInput | void> {
         if (!msg.body) {
             throw new Error("handleText does not have message.body");
         }
@@ -291,7 +256,7 @@ export class WhatsappService {
 
         if (handleTextResult.isSuccess()) {
             const replyText: string = handleTextResult.getData();
-            await this.reply.sendText(msg.from, replyText);
+            await this.sendText(msg.from, replyText);
 
             // const cleaned = this.cleanPrefix(handleTextResult.getData());
             // const json = JSON.parse(cleaned);
@@ -301,11 +266,11 @@ export class WhatsappService {
         }
     }
 
-    private async handleImage(msg: IImageMessage, user: UserData): Promise<void> {
+    private async handleImage(msg: IImageMessage, user: UserData): Promise<ChatInput | void> {
         if (msg.mediaId && msg.url) {
             console.log(`[Whatsapp] Detected message as: Image`);
 
-            const buffer = await this.media.fetch(msg.mediaId, msg.url);
+            const buffer = await this.fetch(msg.mediaId, msg.url);
             console.log(`[Whatsapp] Fetched image buffer from Whatsapp`);
 
             const saveImageResult: Result<MediaData> = await mediaService.saveImage(
@@ -317,49 +282,47 @@ export class WhatsappService {
                 msg.sha256,
             );
             console.log(`[Whatsapp] Saved image to Firestore and Storage`);
-
             if (saveImageResult.isFailure()) {
                 throw new Error(`handleImage failed to saveImage: ${saveImageResult.getMessage()}`);
             }
 
-            const saveImage: MediaData = saveImageResult.getData();
-
-            const handleImageResult: Result<string> = await mainService.handleImage(
-                user.mobile_no,
-                saveImage.mediaName,
-            );
-            if (handleImageResult.isSuccess()) {
-                await this.reply.sendText(msg.from, handleImageResult.getData());
-            } else if (handleImageResult.isFailure()) {
-                await this.reply.sendText(msg.from, handleImageResult.getMessage());
+            const savedImage: MediaData = saveImageResult.getData();
+            const imageResult: Result<MediaData> = await mediaService.getMediaMetaDataByMediaName(savedImage.mediaName);
+            if (imageResult.isFailure()) {
+                throw new Error("handleImage failed to retrieve image.");
             }
+
+            const image: MediaData = imageResult.getData();
+            return {
+                mobile_no: user.mobile_no,
+                created_by: "WHATSAPP",
+                message: msg.caption,
+                media_url: image.download_url,
+                media_name: image.mediaName,
+            };
         }
     }
 
-    private async handleAudio(msg: IAudioMessage, user: UserData): Promise<void> {
-        if (msg.mediaId && msg.url) {
-            const buffer = await this.media.fetch(msg.mediaId, msg.url);
-            const transcript = await whatsappConverter.convertAndTranscribe(buffer);
-            console.log(transcript.success);
-
-            if (transcript.success) {
-                const textMsg: ITextMessage = {
-                    ...msg,
-                    type: "text",
-                    body: transcript.text,
-                };
-                this.handleText(textMsg, user);
-            } else {
-                this.reply.sendText(msg.from, transcript.text);
-            }
+    private async handleAudio(msg: IAudioMessage, user: UserData): Promise<ChatInput | void> {
+        if (!msg.mediaId || !msg.url) {
+            throw new Error("handleAudio URL invalid.");
         }
+
+        const buffer = await this.fetch(msg.mediaId, msg.url);
+        const transcript = await whatsappConverter.convertAndTranscribe(buffer);
+
+        return {
+            mobile_no: user.mobile_no,
+            created_by: "WHATSAPP",
+            message: transcript.text,
+        };
     }
 
-    private async handleVideo(msg: IVideoMessage, user: UserData): Promise<void> {
+    private async handleVideo(msg: IVideoMessage, user: UserData): Promise<ChatInput | void> {
         if (msg.mediaId && msg.url) {
             console.log(`[Whatsapp] Detected message as: Video`);
 
-            const buffer = await this.media.fetch(msg.mediaId, msg.url);
+            const buffer = await this.fetch(msg.mediaId, msg.url);
             console.log(`[Whatsapp] Fetched image buffer from Whatsapp`);
 
             const saveVideoResult: Result<MediaData> = await mediaService.saveVideo(
@@ -373,20 +336,22 @@ export class WhatsappService {
             console.log(`[Whatsapp] Saved video to Firestore and Storage`);
 
             if (saveVideoResult.isFailure()) {
-                throw new Error(`handleImage failed to saveImage: ${saveVideoResult.getMessage()}`);
+                throw new Error(`handleVideo failed to saveVideo: ${saveVideoResult.getMessage()}`);
             }
 
             const savedVideo: MediaData = saveVideoResult.getData();
-
-            const handleVideoResult: Result<string> = await mainService.handleVideo(
-                user.mobile_no,
-                savedVideo.mediaName,
-            );
-            if (handleVideoResult.isSuccess()) {
-                await this.reply.sendText(msg.from, handleVideoResult.getData());
-            } else if (handleVideoResult.isFailure()) {
-                await this.reply.sendText(msg.from, handleVideoResult.getMessage());
+            const imageResult: Result<MediaData> = await mediaService.getMediaMetaDataByMediaName(savedVideo.mediaName);
+            if (imageResult.isFailure()) {
+                throw new Error("handleVideo failed to retrieve video.");
             }
+
+            const image: MediaData = imageResult.getData();
+            return {
+                mobile_no: user.mobile_no,
+                created_by: "WHATSAPP",
+                media_url: image.download_url,
+                media_name: savedVideo.mediaName,
+            };
         }
     }
 
@@ -395,7 +360,7 @@ export class WhatsappService {
             throw new Error("handleLocation undefined longitude or latitude");
         }
 
-        const handleLocationResult: Result<UserData> = await mainService.handleLocation(
+        const handleLocationResult: Result<UserData> = await chatService.handleLocation(
             user.mobile_no,
             msg.latitude,
             msg.longitude,
@@ -404,22 +369,22 @@ export class WhatsappService {
         if (handleLocationResult.isFailure()) {
             throw new Error(`handleLocation failed to updateUserCoords ${handleLocationResult.getMessage()}`);
         } else if (handleLocationResult.isSuccess()) {
-            await this.reply.sendText(msg.from, "Location updated successfully.");
+            await this.sendText(msg.from, "Location updated successfully.");
         }
     }
 
     private async sendIntroductionMessage(message: WhatsappMessage) {
-        await this.reply.sendText(
+        await this.sendText(
             message.from,
             `
-          Welcome to PadiPro! 🌾💪 
-          \nI'm a quick diagnostics tool that offers you guidance on what issues your paddy plants may be facing, and how to solve them!
-          \nYou may respond by:
-          \n1. Uploading an image for us to diagnose and provide you with the recommended solution(s) 🌾 📸 
-          \n2. Ask questions regarding rice plant diseases commonly found in Malaysia ❓ 💬 
-          \n3. Send us your live location for us to determine the local weather and climate in future diagnostics 🌥️ 🌧️ 
-          \nI'm able to respond to both text and image messages, now let's get started!
-        `,
+            Welcome to PadiPro! 🌾💪 
+            \nI'm a quick diagnostics tool that offers you guidance on what issues your paddy plants may be facing, and how to solve them!
+            \nYou may respond by:
+            \n1. Uploading an image for us to diagnose and provide you with the recommended solution(s) 🌾 📸 
+            \n2. Ask questions regarding rice plant diseases commonly found in Malaysia ❓ 💬 
+            \n3. Send us your live location for us to determine the local weather and climate in future diagnostics 🌥️ 🌧️ 
+            \nI'm able to respond to both text and image messages, now let's get started!
+            `,
         );
         return;
     }
@@ -432,14 +397,14 @@ export class WhatsappService {
         }
 
         const locationTutorialImages: LocationTutorialImages = locationTutorialImagesResult.getData();
-        await this.reply.sendText(to, "Before we start, please set up your location by following the steps below:");
-        await this.reply.sendImage(to, { link: locationTutorialImages.step_1 }, "Step 1");
-        await this.reply.sendImage(to, { link: locationTutorialImages.step_2 }, "Step 2");
-        await this.reply.sendImage(to, { link: locationTutorialImages.step_3 }, "Step 3");
+        await this.sendText(to, "Before we start, please set up your location by following the steps below:");
+        await this.sendImage(to, { link: locationTutorialImages.step_1 }, "Step 1");
+        await this.sendImage(to, { link: locationTutorialImages.step_2 }, "Step 2");
+        await this.sendImage(to, { link: locationTutorialImages.step_3 }, "Step 3");
     }
 
     public async sendOTP(to: string, otp: string): Promise<void> {
-        await this.reply.sendText(to, `Your One-Time Password (OTP) is ${otp}. Valid for 5 minutes.`);
+        await this.sendText(to, `Your One-Time Password (OTP) is ${otp}. Valid for 5 minutes.`);
     }
 
     private cleanInternalTag(str: string): string {
