80f3b3f061f1ea9306215059e790acdcfdbc5ed9
iZ86 <iZLikesCS@gmail.com>
2026-05-09 17:20:00 +0800

    Add service ogg file type for audio saveFile

--- app/features/media/media-service.ts
+++ app/features/media/media-service.ts
@@ -158,6 +158,7 @@ class MediaService implements IMediaService {
       "image/gif": ".gif",
       "video/mp4": ".mp4",
       "audio/mpeg": ".mp3",
+      "audio/ogg": ".ogg",
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document": ".docx",
     };
     return map[mimeType] ?? "";
@@ -453,7 +454,7 @@ class MediaService implements IMediaService {
     mobile_no: string,
   ): Promise<Result<MediaFileData>> {
     const ext = this.extFromMime(mimeType);
-    if (ext.length === 0 || ext !== ".mp3") {
+    if (ext.length === 0 || (ext !== ".mp3" && ext !== ".ogg") ) {
       return Result.fail(
         ENUM_STATUS_CODES_FAILURE.UNSUPPORTED_MEDIA_TYPE,
         `File type ${mimeType} not supported for audio uploads.`,
