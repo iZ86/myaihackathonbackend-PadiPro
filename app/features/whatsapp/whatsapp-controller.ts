0a961d0fb37259ebbda2e3861f7e87d1fa886fc7
iZ86 <iZLikesCS@gmail.com>
2026-05-12 03:03:15 +0800

    Remove controller whatsapp handleWebhook
    No longer used

--- app/features/whatsapp/whatsapp-controller.ts
+++ app/features/whatsapp/whatsapp-controller.ts
@@ -6,42 +6,7 @@ import userService from '../user/user-service';
 import { UserData } from '../user/user-model';
 
 export class WhatsappController {
-  async handleWebhook(req: Request<{}, {}, RawWebhookBody>, res: Response): Promise<void> {
-    // Act immediately — WhatsApp retries if no 200 within 20s
-    res.sendStatus(200);
-
-    const value = req.body?.entry?.[0]?.changes?.[0]?.value;
-    if (!value?.messages?.length) return; // status update or empty ping
-
-    const rawMsg = value.messages[0];
-    const contact = value.contacts?.[0];
-    const meta = value.metadata;
-
-
-
-    //check if user exist
-    if (contact && contact.wa_id) {
-      const message = whatsappService.parse(rawMsg!, contact, meta);
-      const mobile_no: string = contact.wa_id;
-
-      let newUser: boolean = false;
-      let userResult: Result<UserData> = await userService.getUserByMobileNo(mobile_no);
-
-      if (userResult.isFailure()) {
-        userResult = await userService.createUser(contact.wa_id, contact.profile.name);
-        newUser = true;
-      }
-
-      if (userResult.isSuccess()) {
-        //call service logic
-
-        const user: UserData = userResult.getData();
-        let locationExist: boolean = !!user.coords;
-
-        await whatsappService.handle(message, userResult.getData(), newUser, locationExist);
-      }
-    }
-  }
+  
 
   async generateOTP(req: Request, res: Response): Promise<void> {
 
